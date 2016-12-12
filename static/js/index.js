2424541482dd3320aa524567ecfea1377b6dd1c3
Leonidas Esteban <leonidasesteban@gmail.com>
2016-12-12 14:11:33 -0500

    [add] first commit

---
+++ static/js/index.js
@@ -0,0 +1,37 @@
+const Elemento = React.createClass({
+  getInitialState() {
+    return {
+      nombre: 'Sammia',
+    }
+  },
+  componentDidMount() {
+    let contador = 0;
+
+
+    setInterval( e => {
+      this.setState({
+        nombre: this.props.nombres[contador],
+      })
+      contador++;
+      if (contador === 6) {
+        contador = 0;
+      }
+    }, 2000);
+  },
+  render() {
+    return (
+      <h1 className="titulo" >Hola {this.state.nombre}!</h1>
+    )
+  }
+});
+
+const nombres = [
+      'Pao',
+      'Sari',
+      'Melissa',
+      'Sofía',
+      'Sheila',
+      'Sammia',
+    ];
+
+ReactDOM.render(<Elemento nombres={nombres}/>, document.getElementById('mi-selector-de-id'));
